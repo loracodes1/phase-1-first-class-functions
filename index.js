fa3c0541a09ca21b616ba943ce43e4ff67daca0b
florence <loracodes3@gmail.com>
2024-10-09 13:53:45 +0300

    final commit

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
- function receivesAFunction(callback){
-    callback()
-}
- 
- function returnsANamedFunction(){
-    return ANamedFunction()
- }
-
- function returnsAnAnonymousFunction(){
-    return function()
- }
+function receivesAFunction(callback) {
+    callback(); // Call the callback function
+  }
+  
+  function returnsANamedFunction() {
+    // Define the named function
+    function namedFunction() {
+      console.log("This is a named function.");
+    }
+    return namedFunction; // Return the named function itself
+  }
+  
+  function returnsAnAnonymousFunction() {
+    return function() {
+      console.log("This is an anonymous function.");
+    };
+  }
+  
